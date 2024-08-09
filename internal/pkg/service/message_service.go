bd247e69f712d3ab9ed5d2257b0968877946f4fa
aarsha-b <boppudi.sai@meesho.com>
2024-08-09 01:17:04 -0700

    Update message_service.go

--- internal/pkg/service/message_service.go
+++ internal/pkg/service/message_service.go
@@ -9,7 +9,7 @@ import (
 	config "notifications/configurations"
 	"notifications/internal/models"
 	"notifications/internal/pkg/repository"
-
+	msg "notifications/internal/kafka"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
@@ -28,7 +28,7 @@ const (
 	ErrParseTimestamp     = "error parsing timestamp"
 )
 
-type Message_Service struct {
+type MessageService struct {
 	db              *repository.MySQLRepo
 	producer        *kafka.Producer
 	kafkaConsumer   *kafka.Consumer
@@ -40,28 +40,49 @@ type Message_Service struct {
 }
 
 var MSGS []models.SMS
-var now time.Time
 
-func New_Message_Service(db *repository.MySQLRepo, producer *kafka.Producer, kafkaConsumer *kafka.Consumer, redisRepo *repository.RedisRepo, esRepo *repository.ElasticsearchRepo) *Message_Service {
-	service := &Message_Service{
-		db:            db,
-		producer:      producer,
-		kafkaConsumer: kafkaConsumer,
-		redisRepo:     redisRepo,
-		esRepo:        esRepo,
+func NewMessageService() *MessageService {
+	sqlrepo,err:= repository.GetMySqlRepository()
+	if err!=nil{
+		log.Panic(err)
+	}
+	produce,err:=msg.GetKafkaProducer()
+	if err!=nil{
+		log.Panic(err)
+	}
+	consume,err:= msg.GetKafkaConsumer()
+	if err!=nil{
+		log.Panic(err)
+	}
+	redisrepo,err:= repository.GetRedisRepository()
+	if err!=nil{
+		log.Panic(err)
+	}
+	elasticrepo,err:= repository.GetElasticRepo()
+	if err!=nil{
+		log.Panic(err)
+	}
+	service := &MessageService{
+		db:            sqlrepo,
+		producer:      produce,
+		kafkaConsumer: consume,
+		redisRepo:     redisrepo,
+		esRepo:        elasticrepo,
 	}
 	go service.StartConsumingMessages()
 	return service
 }
+func GetMessageService()*MessageService{
+return NewMessageService()
+}
+var now time.Time
 
-
-
-func (s *Message_Service) Create_SMS(sms *models.SMS) error {
+func (s *MessageService) CreateMessage(sms *models.SMS) error {
 	now = time.Now().UTC().Add(5*time.Hour + 30*time.Minute)
 	sms.CreatedAt = now
 	sms.UpdatedAt = now
 	if err := s.db.Create(sms); err != nil {
-		log.Printf("Create_SMS: %s: %v", ErrCreateSMSDB, err)
+		log.Printf("CreateMessage: %s: %v", ErrCreateSMSDB, err)
 		return fmt.Errorf("%s: %w", ErrCreateSMSDB, err)
 	}
 	topic := "SMS"
@@ -71,16 +92,16 @@ func (s *Message_Service) Create_SMS(sms *models.SMS) error {
 	}, nil)
 
 	if err != nil {
-		log.Printf("Create_SMS: %s: %v", ErrProduceKafka, err)
+		log.Printf("CreateMessage: %s: %v", ErrProduceKafka, err)
 		return fmt.Errorf("%s: %w", ErrProduceKafka, err)
 	}
 
-	log.Printf("Create_SMS: Successfully created SMS with ID %s and sent to Kafka", sms.ID)
+	log.Printf("CreateMessage: Successfully created SMS with ID %s and sent to Kafka", sms.ID)
 	MSGS = append(MSGS, *sms)
 	return nil
 }
 
-func (s *Message_Service) StartConsumingMessages() {
+func (s *MessageService) StartConsumingMessages() {
 	topic := config.KafkaTopic
 	err := s.kafkaConsumer.SubscribeTopics([]string{topic}, nil)
 	if err != nil {
@@ -101,7 +122,7 @@ func (s *Message_Service) StartConsumingMessages() {
 		log.Printf("StartConsumingMessages: Message received from Kafka and added to queue: %s", msg.Value)
 	}
 }
-func (s *Message_Service) Process_Messages() ([]map[string]interface{}, error) {
+func (s *MessageService) ProcessMessages() ([]map[string]interface{}, error) {
 	s.queueSwitch =!s.queueSwitch
 	var messages []string
 
@@ -116,136 +137,136 @@ func (s *Message_Service) Process_Messages() ([]map[string]interface{}, error) {
 
 	if len(messages) == 0 {
 		log.Println(ErrNoMessages)
-		return s.create_no_messages_response(), nil
+		return s.createNoMessagesResponse(), nil
 	}
 
-	log.Printf("Process_Messages: Processing %d messages", len(messages))
+	log.Printf("ProcessMessages: Processing %d messages", len(messages))
 	var results []map[string]interface{}
 	for _, msgID := range messages {
-		result, err := s.process_message(msgID)
+		result, err := s.processMessage(msgID)
 		if err != nil {
 			return nil, err
 		}
 		results = append(results, result)
 	}
 
-	log.Println("Process_Messages: Message processing complete")
+	log.Println("ProcessMessages: Message processing complete")
 	return results, nil
 }
 
-func (s *Message_Service) create_no_messages_response() []map[string]interface{} {
+func (s *MessageService) createNoMessagesResponse() []map[string]interface{} {
 	return []map[string]interface{}{
 		{"data": map[string]string{"comments": ErrNoMessages}},
 	}
 }
 
-func (s *Message_Service) process_message(msgID string) (map[string]interface{}, error) {
-	exists, err := s.check_sms_existence(msgID)
+func (s *MessageService) processMessage(msgID string) (map[string]interface{}, error) {
+	exists, err := s.checkSmsExistence(msgID)
 	if err != nil {
 		return nil, err
 	}
 	if !exists {
-		return s.create_sms_not_found_response(msgID), nil
+		return s.createSmsNotFoundResponse(msgID), nil
 	}
 
-	sms, err := s.retrieve_sms_details(msgID)
+	sms, err := s.retrieveSmsDetails(msgID)
 	if err != nil {
 		return nil, err
 	}
 
 	ctx := context.Background()
-	blacklist, err := s.check_blacklist_status(ctx, sms.PhoneNumber)
+	blacklist, err := s.checkBlacklistStatus(ctx, sms.PhoneNumber)
 	if err != nil {
 		return nil, err
 	}
 
 	if blacklist {
-		return s.handle_blacklisted_sms(sms)
+		return s.handleBlacklistedSms(sms)
 	}
 
-	return s.handle_successful_sms(sms)
+	return s.handleSuccessfulSms(sms)
 }
 
-func (s *Message_Service) check_sms_existence(msgID string) (bool, error) {
+func (s *MessageService) checkSmsExistence(msgID string) (bool, error) {
 	var exists bool
 	existQuery := `SELECT EXISTS(SELECT 1 FROM sms WHERE id = ?)`
 	err := s.db.Raw(existQuery, msgID).Row().Scan(&exists)
 	if err != nil {
-		log.Printf("check_sms_existence: %s for %s: %v", ErrCheckIDExistence, msgID, err)
+		log.Printf("checkSmsExistence: %s for %s: %v", ErrCheckIDExistence, msgID, err)
 		return false, fmt.Errorf("%s: %w", ErrCheckIDExistence, err)
 	}
 	return exists, nil
 }
 
-func (s *Message_Service) create_sms_not_found_response(msgID string) map[string]interface{} {
+func (s *MessageService) createSmsNotFoundResponse(msgID string) map[string]interface{} {
 	return map[string]interface{}{
 		"error": map[string]string{"comments": msgID},
 	}
 }
 
-func (s *Message_Service) retrieve_sms_details(msgID string) (models.SMS, error) {
+func (s *MessageService) retrieveSmsDetails(msgID string) (models.SMS, error) {
 	var sms models.SMS
 	query := `SELECT id, phone_number, message FROM sms WHERE id = ?`
 	err := s.db.Raw(query, msgID).Scan(&sms).Error
 	if err != nil {
-		log.Printf("retrieve_sms_details: %s for %s: %v", ErrRetrieveSMSDetails, msgID, err)
+		log.Printf("retrieveSmsDetails: %s for %s: %v", ErrRetrieveSMSDetails, msgID, err)
 		return sms, fmt.Errorf("%s: %w", ErrRetrieveSMSDetails, err)
 	}
 	return sms, nil
 }
 
-func (s *Message_Service) check_blacklist_status(ctx context.Context, phoneNumber string) (bool, error) {
+func (s *MessageService) checkBlacklistStatus(ctx context.Context, phoneNumber string) (bool, error) {
 	blacklist, err := s.redisRepo.SIsMember(ctx, "Black", phoneNumber).Result()
 	if err != nil {
-		log.Printf("check_blacklist_status: %s for %s: %v", ErrBlacklistCheck, phoneNumber, err)
+		log.Printf("checkBlacklistStatus: %s for %s: %v", ErrBlacklistCheck, phoneNumber, err)
 		return false, fmt.Errorf("%s: %w", ErrBlacklistCheck, err)
 	}
 	return blacklist, nil
 }
 
-func (s *Message_Service) handle_blacklisted_sms(sms models.SMS) (map[string]interface{}, error) {
+func (s *MessageService) handleBlacklistedSms(sms models.SMS) (map[string]interface{}, error) {
 	sms.Status = "Failed"
 	sms.FailureComments = "Blacklisted number"
 	err := s.db.UpdateSMSStatus(sms.ID, sms.Status, sms.FailureComments)
 	if err != nil {
-		log.Printf("handle_blacklisted_sms: %s for %s: %v", ErrUpdateSMSStatus, sms.ID, err)
+		log.Printf("handleBlacklistedSms: %s for %s: %v", ErrUpdateSMSStatus, sms.ID, err)
 		return nil, fmt.Errorf("%s: %w", ErrUpdateSMSStatus, err)
 	}
 
-	err = s.index_sms_in_elasticsearch(sms)
+	err = s.indexSmsInElasticsearch(sms)
 	if err != nil {
-		log.Printf("handle_blacklisted_sms: %s for %s: %v", ErrIndexElasticsearch, sms.ID, err)
+		log.Printf("handleBlacklistedSms: %s for %s: %v", ErrIndexElasticsearch, sms.ID, err)
 		return nil, fmt.Errorf("%s: %w", ErrIndexElasticsearch, err)
 	}
 
-	log.Printf("handle_blacklisted_sms: SMS ID %s is blacklisted", sms.ID)
+	log.Printf("handleBlacklistedSms: SMS ID %s is blacklisted", sms.ID)
 	return map[string]interface{}{
 		"data": map[string]string{"comments": "Blacklisted number"},
 	}, nil
 }
 
-func (s *Message_Service) handle_successful_sms(sms models.SMS) (map[string]interface{}, error) {
+func (s *MessageService) handleSuccessfulSms(sms models.SMS) (map[string]interface{}, error) {
 	sms.Status = "Successful"
 	sms.FailureComments = "No failure comments"
 	err := s.db.UpdateSMSStatus(sms.ID, sms.Status, sms.FailureComments)
 	if err != nil {
-		log.Printf("handle_successful_sms: %s for %s: %v", ErrUpdateSMSStatus, sms.ID, err)
+		log.Printf("handleSuccessfulSms: %s for %s: %v", ErrUpdateSMSStatus, sms.ID, err)
 		return nil, fmt.Errorf("%s: %w", ErrUpdateSMSStatus, err)
 	}
 
-	err = s.index_sms_in_elasticsearch(sms)
+	err = s.indexSmsInElasticsearch(sms)
 	if err != nil {
-		log.Printf("handle_successful_sms: %s for %s: %v", ErrIndexElasticsearch, sms.ID, err)
+		log.Printf("handleSuccessfulSms: %s for %s: %v", ErrIndexElasticsearch, sms.ID, err)
 		return nil, fmt.Errorf("%s: %w", ErrIndexElasticsearch, err)
 	}
 
-	log.Printf("handle_successful_sms: SMS ID %s processed successfully", sms.ID)
+	log.Printf("handleSuccessfulSms: SMS ID %s processed successfully", sms.ID)
 	return map[string]interface{}{
 		"data": map[string]string{"comments": "Successfully processed"},
 	}, nil
 }
 
-func (s *Message_Service) index_sms_in_elasticsearch(sms models.SMS) error {
+func (s *MessageService) indexSmsInElasticsearch(sms models.SMS) error {
 	t := time.Now().UTC().Add(5*time.Hour + 30*time.Minute)
 	dur := t.Sub(now)
 	err := s.esRepo.CreateIndexSMS(sms, dur)
@@ -255,26 +276,26 @@ func (s *Message_Service) index_sms_in_elasticsearch(sms models.SMS) error {
 	return nil
 }
 
-func (s *Message_Service) Check_ID_Exists(msgID string) (bool, error) {
+func (s *MessageService) CheckIDExists(msgID string) (bool, error) {
 	var exists bool
 
 	// Query to check if the SMS ID exists
 	existQuery := `SELECT EXISTS(SELECT 1 FROM sms WHERE id = ?)`
 	err := s.db.Raw(existQuery, msgID).Row().Scan(&exists)
 	if err != nil {
-		log.Printf("Check_ID_Exists: Failed to check ID existence for %s: %v", msgID, err)
+		log.Printf("CheckIDExists: Failed to check ID existence for %s: %v", msgID, err)
 		return false, fmt.Errorf("failed to check ID existence: %w", err)
 	}
 
 	return exists, nil
 }
 
-func (s *Message_Service) Get_All_Messages() ([]models.SMS, error) {
+func (s *MessageService) GetAllMessages() ([]models.SMS, error) {
 	query := `SELECT id, phone_number, message, status, failure_code, failure_comments, created_at, updated_at FROM sms`
 
 	rows, err := s.db.Raw(query).Rows()
 	if err != nil {
-		log.Printf("Get_All_Messages: Failed to retrieve SMS details: %v", err)
+		log.Printf("GetAllMessages: Failed to retrieve SMS details: %v", err)
 		return nil, fmt.Errorf("failed to retrieve SMS details: %w", err)
 	}
 	defer rows.Close()
@@ -285,13 +306,13 @@ func (s *Message_Service) Get_All_Messages() ([]models.SMS, error) {
 		var createdAt, updatedAt []uint8
 
 		if err := rows.Scan(&sms.ID, &sms.PhoneNumber, &sms.Message, &sms.Status, &sms.FailureCode, &sms.FailureComments, &createdAt, &updatedAt); err != nil {
-			log.Printf("Get_All_Messages: Error scanning row: %v", err)
+			log.Printf("GetAllMessages: Error scanning row: %v", err)
 			return nil, fmt.Errorf("error scanning row: %w", err)
 		}
 
-		parsedCreatedAt, parsedUpdatedAt, err := s.parse_Timestamps(createdAt, updatedAt)
+		parsedCreatedAt, parsedUpdatedAt, err := s.parseTimestamps(createdAt, updatedAt)
 		if err != nil {
-			log.Printf("Get_All_Messages: %v", err)
+			log.Printf("GetAllMessages: %v", err)
 			return nil, err
 		}
 
@@ -303,7 +324,7 @@ func (s *Message_Service) Get_All_Messages() ([]models.SMS, error) {
 
 	return result, nil
 }
-func (s *Message_Service) parse_Timestamps(createdAt, updatedAt []uint8) (time.Time, time.Time, error) {
+func (s *MessageService) parseTimestamps(createdAt, updatedAt []uint8) (time.Time, time.Time, error) {
 	var parsedCreatedAt, parsedUpdatedAt time.Time
 
 	var err error
@@ -323,7 +344,7 @@ func (s *Message_Service) parse_Timestamps(createdAt, updatedAt []uint8) (time.T
 
 	return parsedCreatedAt, parsedUpdatedAt, nil
 }
-func (s *Message_Service) Get_Message_By_ID(msgID string) (*models.SMS, error) {
+func (s *MessageService) GetMessageByID(msgID string) (*models.SMS, error) {
 	var sms models.SMS
 	var createdAt, updatedAt []uint8
 
@@ -339,13 +360,13 @@ func (s *Message_Service) Get_Message_By_ID(msgID string) (*models.SMS, error) {
 		&updatedAt,
 	)
 	if err != nil {
-		log.Printf("Get_Message_By_ID: Failed to retrieve SMS details for %s: %v", msgID, err)
+		log.Printf("GetMessageByID: Failed to retrieve SMS details for %s: %v", msgID, err)
 		return nil, fmt.Errorf("failed to retrieve SMS details: %w", err)
 	}
 
-	parsedCreatedAt, parsedUpdatedAt, err := s.parse_Timestamps(createdAt, updatedAt)
+	parsedCreatedAt, parsedUpdatedAt, err := s.parseTimestamps(createdAt, updatedAt)
 	if err != nil {
-		log.Printf("Get_Message_By_ID: %v", err)
+		log.Printf("GetMessageByID: %v", err)
 		return nil, err
 	}
 
