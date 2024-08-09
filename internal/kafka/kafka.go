bdc605e581b645c3e6bda6c974ae342c59a76098
aarsha-b <boppudi.sai@meesho.com>
2024-08-09 01:14:00 -0700

    Update kafka.go

--- internal/kafka/kafka.go
+++ internal/kafka/kafka.go
@@ -2,14 +2,14 @@ package kafka
 
 import (
 	"log"
-
+	"notifications/configurations"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 type Producer = kafka.Producer
 type Consumer = kafka.Consumer
 
-func New_Consumer(brokers, groupID string) (*Consumer, error) {
+func NewConsumer(brokers, groupID string) (*Consumer, error) {
 	log.Println("returning new consumer of kafka")
 	return kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": brokers,
@@ -18,9 +18,25 @@ func New_Consumer(brokers, groupID string) (*Consumer, error) {
 	})
 }
 
-func New_Producer(brokers string) (*Producer, error) {
+func NewProducer(brokers string) (*Producer, error) {
 	log.Println("returning new producer of kafka")
 	return kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": brokers,
 	})
+}
+func GetKafkaProducer()(*kafka.Producer,error){
+	// Initialize Kafka producer
+	producer, err := NewProducer(config.KafkaAddr)
+	if err != nil {
+		return nil, err
+	}
+	return producer,nil
+}
+func GetKafkaConsumer()(*kafka.Consumer,error){
+	// Initialize Kafka producer
+	consumer, err := NewConsumer(config.KafkaAddr,config.KafkaTopic)
+	if err != nil {
+		return nil, err
+	}
+	return consumer,nil
 }
